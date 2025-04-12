5235cbde3a21058811a34d573b7468ec6c226ba3
ChedlyRebai <chedly.rebai123@gmail.com>
2025-04-12 10:14:02 +0100

    feat: refactor AuthForm and FormField components for improved structure and usability

--- compnents/Forms/AuthForm.tsx
+++ compnents/Forms/AuthForm.tsx
@@ -8,16 +8,25 @@ import {
     Form,
     FormControl,
     FormDescription,
-    FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import Image from "next/image";
+import FormField from "./FormField";
+import Link from "next/link";
+
+
 const formSchema = z.object({
-    username: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
+    name: z.string().min(2, {
+        message: "Name must be at least 2 characters.",
+    }),
+    email: z.string().email({
+        message: "Please enter a valid email address.",
+    }),
+    password: z.string().min(8, {
+        message: "Password must be at least 8 characters.",
     }),
 })
 
@@ -26,15 +35,17 @@ const AuthForm = ({ type }: { type: string }) => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            username: "",
+            name: "",
+            email: "",
+            password: "",
         },
     })
 
     function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+       
     }
+
+    const isSignIn = type === "sign-in"
     return (
         <div className="card-border lg:min-w-[566px]">
             <div className="flex flex-col gap-6 card py-16 px-10">
@@ -43,20 +54,55 @@ const AuthForm = ({ type }: { type: string }) => {
                         src="/logo.svg"
                         alt="logo"
                         height={32}
-                        width={32}  
+                        width={32}
                     />
                     <h2 className="text-primary-100">AiInterview</h2>
                 </div>
                 <h3>Practice job interview</h3>
 
-            
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} 
-                className="w-full space-y-6 mt-4 form">
-                    
-                    <Button type="submit">Submit</Button>
-                </form>
-            </Form>
+
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}
+                        className="w-full space-y-6 mt-4 form">
+                        {!isSignIn && (
+                            <FormField
+                                name="name"
+                                label="Name"
+                                control={form.control}
+                                type="text"
+                                placeholder="Enter your name"
+                            />
+                        )}
+
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            label="Email"
+                            placeholder="Your email address"
+                            type="email"
+                        />
+
+                        <FormField
+                            name="password"
+                            label="Password"
+                            control={form.control}
+                            type="password"
+                            placeholder="Enter your password"
+                        />
+                        <Button className="btn" type="submit">
+                            {isSignIn ? "Sign In" : "Create an Account"}
+                        </Button>
+                    </form>
+                </Form>
+                <p className="text-center">
+                    {isSignIn ? "No account yet?" : "Have an account already?"}
+                    <Link
+                        href={!isSignIn ? "/sign-in" : "/sign-up"}
+                        className="font-bold text-user-primary ml-1"
+                    >
+                        {!isSignIn ? "Sign In" : "Sign Up"}
+                    </Link>
+                </p>
             </div>
         </div>
     );
--- compnents/Forms/FormField.tsx
+++ compnents/Forms/FormField.tsx
@@ -1,14 +1,18 @@
-import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
 
-import React from "react";
-import { Control, Controller, Field, FieldValues, Path } from "react-hook-form";
+import {
+    FormItem,
+    FormLabel,
+    FormControl,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface FormFieldProps<T extends FieldValues> {
     control: Control<T>;
     name: Path<T>;
     label: string;
-    plcaceholder: string;
+    placeholder?: string;
     type?: "text" | "email" | "password";
 }
 
@@ -16,30 +20,29 @@ const FormField = <T extends FieldValues>({
     control,
     name,
     label,
-    plcaceholder,
+    placeholder,
     type = "text",
-}: FormFieldProps<T>) => (
-    <Controller
-        control={control}
-        name={name}
-        render={({ field }) => (
-            <FormItem className="w-full">
-                <FormLabel>{label}</FormLabel>
-                <FormControl>
-                    <Input
-                        {...field}
-                        type={type}
-                        placeholder={plcaceholder}
-                        {...field}
-
-                    />
-                </FormControl>
-                <FormDescription>{plcaceholder}</FormDescription>
-                <FormMessage />
-            </FormItem>
-        )}
-    />
-
-)
+}: FormFieldProps<T>) => {
+    return (
+        <Controller
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <FormItem>
+                    <FormLabel className="label">{label}</FormLabel>
+                    <FormControl>
+                        <Input
+                            className="input"
+                            type={type}
+                            placeholder={placeholder}
+                            {...field}
+                        />
+                    </FormControl>
+                    <FormMessage />
+                </FormItem>
+            )}
+        />
+    );
+};
 
 export default FormField;
