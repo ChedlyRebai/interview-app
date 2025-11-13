08a1860baffd882138c25b121299bb9bde451e2f
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2025-11-13 16:10:14 +0100

    feat: Update Agent component to accept userName prop and enhance avatar display

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,17 +1,41 @@
 import Image from "next/image";
 import React from "react";
 
-const Agent = () => {
-    const isSpeaker = true; // This should be determined by your application logic
+const Agent = ({ userName }: AgentProps) => {
+  const isSpeaker = true; // This should be determined by your application logic
   return (
-    <div className="call-view">
+    <>
+      <div className="call-view">
         <div className="card-interview">
-            <div className="avatar">
-                <Image src="/ai-avatar.png" className="object-cover" alt ="vapi" width={50} height={50} />
-                {isSpeaker && <span className="animate-speak">Speaker</span>}
-            </div>
+          <div className="avatar">
+            <Image
+              src="/ai-avatar.png"
+              className="object-cover"
+              alt="vapi"
+              width={65}
+              height={54}
+            />
+            {isSpeaker && <span className="animate-speak">Speaker</span>}
+          </div>
+
+          <h3>AI interviewer</h3>
         </div>
-    </div>
+
+        <div className="card-border">
+          <div className="card-content">
+            <Image
+              src={"/user-avatar.png"}
+              alt="user avatar"
+              width={540}
+              height={540}
+              className="rounded-full object-cover size-[120px]"
+            />
+
+            <h3>{userName}</h3>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
